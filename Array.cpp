1b835b1808501b72c0f15d9ffe9cd0098953e67b
HedgehogFrog <ben20041208@gmail.com>
2025-03-15 11:15:19 -0700

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-    const int SIZE = 5;  // 陣列大小
-    int numbers[SIZE];   // 宣告整數陣列
+    const int SIZE = 5;  // Array size
+    int numbers[SIZE];   // Declare an integer array
 
-    // 輸入數字
-    cout << "請輸入 " << SIZE << " 個數字：" << endl;
+    // Input numbers
+    cout << "Please enter " << SIZE << " numbers:" << endl;
     for (int i = 0; i < SIZE; i++) {
-        cout << "輸入第 " << i + 1 << " 個數字: ";
+        cout << "Enter number " << i + 1 << ": ";
         cin >> numbers[i];
     }
 
-    // 輸出數字
-    cout << "你輸入的數字是：" << endl;
+    // Output numbers
+    cout << "The numbers you entered are: ";
     for (int i = 0; i < SIZE; i++) {
         cout << numbers[i] << " ";
     }
